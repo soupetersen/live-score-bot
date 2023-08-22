c6f4600ba4bc16f9c16accea3a950ac699b72e4c
soupetersen <soupetersen@gmail.com>
2023-08-22 19:14:43 -0300

    Add track for Sul Americana 2023

--- src/config/watched-championships.ts
+++ src/config/watched-championships.ts
@@ -4,6 +4,7 @@ type WatchedChampionships = {
 
 export const watchedChampionships: WatchedChampionships = {
   "Libertadores 2023": 839,
+  "Sul Americana 2023": 853,
   "Brasileirão Série A 2023": 850,
   "Brasileirão Série B 2023": 851,
   "Copa do Brasil 2023": 845,
