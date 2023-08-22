ba09cb9d45863abcb0049945c9db72b5103ecc38
soupetersen <soupetersen@gmail.com>
2023-08-21 22:45:43 -0300

    Some fixes

--- src/twitter/index.ts
+++ src/twitter/index.ts
@@ -1,19 +1,28 @@
-import { ApiRequestError, TwitterApi } from "twitter-api-v2";
+import { TwitterApi } from "twitter-api-v2";
 
 import "dotenv/config";
 
 export async function postTweet(tweet: string) {
   console.log("POST TWEET", tweet);
 
-  const client = await twitterCLient();
+  const client = await twitterCLient().catch(() => undefined);
+
+  if (!client) {
+    console.log("error", "client not found");
+    return;
+  }
+
   try {
     return await client.v2.tweet(tweet);
   } catch (error) {
-    console.error("error #%d", error);
+    console.log("error sending tweet", error);
   }
 }
 
-export async function twitterCLient(): Promise<TwitterApi> {
+export async function twitterCLient(): Promise<TwitterApi | undefined> {
+  const able = process.env.NODE_ENV !== "test";
+  if (!able) return;
+
   return new TwitterApi({
     appKey: process.env.TWITTER_API_KEY ?? "",
     appSecret: process.env.TWITTER_API_SECRET ?? "",
