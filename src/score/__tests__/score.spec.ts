2aa0ea546f275642ec761f77d6d3c002524f1b08
soupetersen <soupetersen@gmail.com>
2023-08-22 22:09:13 -0300

    Fix tests

--- src/score/__tests__/score.spec.ts
+++ src/score/__tests__/score.spec.ts
@@ -18,23 +18,23 @@ describe("Score", () => {
   });
 
   it("should be cache matches", () => {
-    score.setCacheByChampionshipId(1, matchesMock);
+    score.setCacheByChampionshipId(850, matchesMock);
 
-    expect(score.getCacheByChampionshipId(1)).toEqual(matchesMock);
+    expect(score.getCacheByChampionshipId(850)).toEqual(matchesMock);
   });
 
   it("should be clear cache matches", () => {
-    score.setCacheByChampionshipId(1, matchesMock);
-    score.clearChampionshipCache(1);
+    score.setCacheByChampionshipId(850, matchesMock);
+    score.clearChampionshipCache(850);
 
-    expect(score.getCacheByChampionshipId(1)).toBeUndefined();
+    expect(score.getCacheByChampionshipId(850)).toBeUndefined();
   });
 
   it("should be add new match", async () => {
-    score.setCacheByChampionshipId(1, matchesMock);
+    score.setCacheByChampionshipId(850, matchesMock);
     const result = await updateChampionsipMatches(
-      "Campeonato brasileiro",
-      1,
+      850,
+      "Brasileirão Série A 2023",
       updatedMatchesMock,
     );
 
@@ -42,29 +42,29 @@ describe("Score", () => {
   });
 
   it("should be compare score and return if have team scored", async () => {
-    score.setCacheByChampionshipId(1, matchesMock);
+    score.setCacheByChampionshipId(850, matchesMock);
     const result = await updateChampionsipMatches(
-      "Campeonato brasileiro",
-      1,
+      850,
+      "Brasileirão Série A 2023",
       updatedMatchesMock,
     );
 
     expect(result).toBeDefined();
 
     await compareMatchesScores(
-      1,
-      "Campeonato brasileiro",
+      850,
+      "Brasileirão Série A 2023",
       result!,
       updatedMatchesMock,
     );
   });
 
   it("should sendm match fineshed alert", async () => {
-    score.setCacheByChampionshipId(1, matchesMock);
+    score.setCacheByChampionshipId(850, matchesMock);
 
     await updateChampionsipMatches(
-      "Campeonato brasileiro",
-      1,
+      850,
+      "Brasileirão Série A 2023",
       finishMatchesMock,
     );
   });
